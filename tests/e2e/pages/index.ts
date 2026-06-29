615775d854c122e5da077bf38186351749c329d2
Sthirs <sthirs.code@gmail.com>
2026-06-29 22:11:48 +0200

    test(playwright): add e2e tests for primary use cases

---
+++ tests/e2e/pages/index.ts
@@ -0,0 +1,4 @@
+export { LoginPage } from './LoginPage';
+export { GroupsPage } from './GroupsPage';
+export { GroupSettingsPage } from './GroupSettingsPage';
+export { GroupDetailPage } from './GroupDetailPage';
